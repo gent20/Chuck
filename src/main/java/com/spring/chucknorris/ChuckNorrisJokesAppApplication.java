6955b92893526a3528a17b282adccb44a9e10ab2
gent20 <123qweasdbnM>
2020-06-24 18:00:12 +0200

    chuck norris joke generator

---
+++ src/main/java/com/spring/chucknorris/ChuckNorrisJokesAppApplication.java
@@ -0,0 +1,13 @@
+package com.spring.chucknorris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckNorrisJokesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChuckNorrisJokesAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/chucknorris/ChuckNorrisJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.chucknorris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckNorrisJokesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
